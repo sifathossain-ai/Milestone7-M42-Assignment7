00acf58df206e131c9544022fafdd6d238e2f0f1
Sifat Hossain <sifathossain1326@gmail.com>
2025-08-03 18:32:06 +0530

    Add Claim Free Credit Button

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -14,7 +14,7 @@ const Header = () => {
             <div className='flex justify-between items-center mb-7'>
                 <img className='w-10 h-10 md:w-16 md:h-16' src={logo} alt="" />
                 <div className='flex justify-center items-center gap-2 md:gap-9'>
-                    <ul className='flex justify-center items-center gap-2 md:gap-9 text-[10px] md:text-[16px] text-gray-600'>
+                    <ul className='flex justify-center items-center gap-2 md:gap-9 text-[11px] md:text-[16px] text-gray-600'>
                         <li><a href="">Home</a></li>
                         <li><a href="">Fixture</a></li>
                         <li><a href="">Teams</a></li>
@@ -28,8 +28,8 @@ const Header = () => {
             </div>
             <div className='w-full h-[545px] rounded-lg bg-gradient-to-tr from-[#636b86] to-[#c299a2] text-center flex flex-col items-center justify-center space-y-4'>
                 <img className='w-40 md:w-60' src={banner} alt="Banner" />
-                <h2 className='text-2xl md:text-4xl font-semibold text-white'>Assemble Your Ultimate Dream 11 Cricket Team </h2>
-                <p className='text-gray-300 md:text-xl'>Beyond Boundaries Beyond Limits</p>
+                <h2 className='text-2xl md:text-4xl font-bold text-white'>Assemble Your Ultimate Dream 11 Cricket Team </h2>
+                <p className='text-gray-300 text-sm md:text-xl font-semibold'>Beyond Boundaries Beyond Limits</p>
                 <div className='border p-1 rounded-xl border-[#E7FE29]'>
                     <button onClick={addCoin} className='text-[12px] md:text-[16px] px-4 py-2 rounded-xl bg-[#E7FE29] text-black font-semibold'> Claim Free Credit
                     </button>
