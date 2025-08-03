e00d73496f90c99c3b16d277254d2aad3e1dae03
Sifat Hossain <sifathossain1326@gmail.com>
2025-08-03 18:23:08 +0530

    Add more funtionalities

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Header from './components/Header/Header'
 function App() {
 
   return (
-    <div className='max-w-7xl mx-auto py-7 px-2'> 
+    <div className='max-w-7xl mx-auto py-7 px-2'>
       <Header></Header>
     </div>
   )
